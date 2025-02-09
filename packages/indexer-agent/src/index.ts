3933b288a5ec351cf4240d51d138adcb281cb5e8
polus-arcticus <thulsmans.1133@gmail.com>
2025-02-08 23:23:52 -0800

    cdp

--- packages/indexer-agent/src/index.ts
+++ packages/indexer-agent/src/index.ts
@@ -6,7 +6,11 @@ import { Document } from '@langchain/core/documents'
 import { PGVectorStore } from '@langchain/community/vectorstores/pgvector'
 import { getCurrentBlock } from './utils/chain'
 import prisma from './services/prisma'
-import { GetMergedPositionsDocument, execute } from '../.graphclient'
+if (process.env.NODE_ENV === 'production') {  
+  import { GetMergedPositionsDocument, execute } from './.graphclient'
+} else {
+  import { GetMergedPositionsDocument, execute } from '../.graphclient'
+}
 import { Cohere } from '@langchain/cohere'
 import { CohereEmbeddings } from '@langchain/cohere'
 
