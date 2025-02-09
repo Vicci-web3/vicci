0b60b4c01ba9db2295d6782b8de4946ca26c0b79
polus-arcticus <thulsmans.1133@gmail.com>
2025-02-08 23:10:22 -0800

    cdp

--- packages/api/src/agent/VenueAgent.ts
+++ packages/api/src/agent/VenueAgent.ts
@@ -243,7 +243,7 @@ export default class VenueAgent {
             */
             const agentConfig = this.getAgentConfig();
             const agentkit = await AgentKit.from({
-                walletProvider,
+                walletProvider: this.walletProvider,
                 cdpApiKeyName: process.env.CDP_API_KEY_NAME,
                 cdpApiKeyPrivateKey: process.env.CDP_API_KEY_PRIVATE_KEY,
                 actionProviders: agentConfig.actionProviders,
